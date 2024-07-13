a3b461379f15002589dc3167b1a8101b6918f521
manhNC2k4 <manhnc2k4@gmail.com>
2024-07-13 22:10:50 +0700

    create project

---
+++ src/main/java/com/example/shopmayja/ShopMayJaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopmayja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMayJaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMayJaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shopmayja/ShopMayJaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopmayja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMayJaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
